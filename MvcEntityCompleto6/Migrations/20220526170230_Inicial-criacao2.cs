a884ff4bbdafba3df4a9c2cd358671319d1451d9
Samuel Aleixo <samuelmatriz@hotmail.com>
2022-05-27 11:24:57 -0300

    criação nova Branch

---
+++ MvcEntityCompleto6/Migrations/20220526170230_Inicial-criacao2.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcEntityCompleto6.Migrations
+{
+    public partial class Inicialcriacao2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transporte",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Responsavel = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transporte", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transporte");
+        }
+    }
+}
---
+++ MvcEntityCompleto6/Models/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using MvcEntityCompleto6.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace MvcEntityCompleto6.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+        public DbSet<Pessoa> Pessoa { get; set; }
+
+        public DbSet<Transporte> Transporte { get; set; }
+
+    }
+}
