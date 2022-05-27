2c99483cfaf68b8f03a41d29e295e5b27188dbfa
Samuel Aleixo <samuelmatriz@hotmail.com>
2022-05-27 11:24:57 -0300

    Adicionar arquivos de projeto.

---
+++ MvcEntityCompleto6/Models/Pessoa.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace MvcEntityCompleto6.Models
+{
+    [Table("Pessoa")]
+    public class Pessoa
+    {
+        [Column("Id")]
+        [Display(Name = "Codigo")]
+
+        public int Id { get; set; }
+
+
+
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+
+
+        [Column("Email")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+
+
+        [Column("Salario")]
+        [Display(Name = "Salario")]
+        public double Salario { get; set; }
+
+
+        [Column("Apelido")]
+        [Display(Name = "Apelido")]
+        public string Apelido { get; set; }
+    }
+}
+
---
+++ MvcEntityCompleto6/Models/Transporte.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+namespace MvcEntityCompleto6.Models
+{
+    [Table("Transporte")]
+    public class Transporte
+    {
+        [Column("Id")]
+        [Display(Name = "Codigo")]
+
+        public int Id { get; set; }
+
+
+
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+
+
+        [Column("Responsavel")]
+        [Display(Name = "Responsavel")]
+        public string Responsavel { get; set; }
+    }
+}
